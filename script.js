aa2a5ed729f25c310bbdfaa2116d13939c03f9af
Kamela <kami63116@yahoo.com>
2024-04-05 19:47:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function check() {
 
     //conditionals
     if(halloween_movies === "halloween") {
-        document.querySelector("#placeToSee").innerText = "But you can't kill the boogeyman!";
-        document.querySelector("body").style.backgroundImage = "url('https://i.pinimg.com/originals/c3/62/df/c362dfc187fdb1a63f82975584e92716.gif')";
+        document.querySelector("#placeToSee").innerText = "";
+        document.querySelector("body").style.backgroundImage = "url('https://i.pinimg.com/originals/f0/ae/8d/f0ae8ded936bc4b0d02480e3bbd1977d.gif')";
     } else if (halloween_movies === "the craft") {
         document.querySelector("#placeToSee").innerText = "";
         document.querySelector("body").style.backgroundImage = "url('https://media1.popsugar-assets.com/files/thumbor/F4Wat2f4z_gMx9DgUOr_EhIpcmI/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/04/30/668/n/1922283/4bc1149d43601cd7_giphy1/i/Nancy-Really-Really-Scary.gif')";
